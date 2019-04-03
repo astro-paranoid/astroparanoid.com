0af1635646ac1acaa5017dd3176d98ec9cafccad
Jagdeep Singh <jagdeeeeep@gmail.com>
2019-04-02 20:57:51 -0700

    map works

--- public/js/app.js
+++ public/js/app.js
@@ -1,97 +1,50 @@
+'use strict';
 
-var citymap = {
-  chicago: {
-    center: {lat: 41.878, lng: -87.629},
-    population: 2714856
+let citymap = {
+  seattle: {
+    center: {lat: 47.6062, lng: -122.3321},
   },
-  newyork: {
-    center: {lat: 40.714, lng: -74.005},
-    population: 8405837
-  },
-  losangeles: {
-    center: {lat: 34.052, lng: -118.243},
-    population: 3857799
-  },
-  vancouver: {
-    center: {lat: 49.25, lng: -123.1},
-    population: 603502
-  }
+};
+
+let crater = {
+  min: 800,
+  max: 1000,
 };
 
 function initMap() {
   // Create the map.
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: {lat: 37.090, lng: -95.712},
-    mapTypeId: 'terrain'
+  let map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: citymap[Object.keys(citymap)[0]].center, // {lat: 47.6062, lng: -122.3321},
+    mapTypeId: 'hybrid',
+    disableDefaultUI: true,
+    zoomControl: true,
+
   });
 
   // Construct the circle for each value in citymap.
-  // Note: We scale the area of the circle based on the population.
-  for (var city in citymap) {
+  for (let city in citymap) {
     // Add the circle for this city to the map.
-    var cityCircle = new google.maps.Circle({
+    let minCraterCircle = new google.maps.Circle({
       strokeColor: '#FF0000',
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
+      strokeOpacity: 0.5,
+      strokeWeight: 1,
       fillColor: '#FF0000',
-      fillOpacity: 0.35,
+      fillOpacity: 0.15,
       map: map,
       center: citymap[city].center,
-      radius: Math.sqrt(citymap[city].population) * 100
+      radius: crater['min'],
+    });
+
+    let maxCraterCircle = new google.maps.Circle({
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.5,
+      strokeWeight: 1,
+      fillColor: '#FF0000',
+      fillOpacity: 0.25,
+      map: map,
+      center: citymap[city].center,
+      radius: crater['max'],
     });
   }
 }
-
-
-
-
-
-
-// console.log('we are connected');
-
-// var cityMap = {
-//   seattle: {
-//     center: {lat: 47.6062, lng: -122.3321},
-//     population: 724750
-//   },
-//   newYork: {
-//     center: {lat: 40.714, lng: -74.005},
-//     population: 8623000
-//   },
-//   london: {
-//     center: {lat: 51.5074, lng: -0.1278},
-//     population: 8787892
-//   },
-//   tokyo: {
-//     center: {lat: 35.6895, lng: 139.69171},
-//     population: 9273000
-//   }
-// };
-
-// initMap();
-
-// function initMap () {
-//   var map = new google.maps.Map(document.getElementById('map'), {
-//     zoom: 4,
-//     center: {lat: 37.090, lng: -95.713},
-//     mapTypeId: 'terrain'
-//   });
-
-//   console.log('hello from for');
-//   for (var city in cityMap) {
-//     var cityCircle = new google.maps.Circle({
-//       strokeColor: '#FF0000',
-//       strokeOpacity: 0.8,
-//       strokeWeight: 2,
-//       fillColor: '#FF0000',
-//       fillOpacity: 0.35,
-//       map: map,
-//       center: cityMap[city].center,
-//       radius: Math.sqrt(cityMap[city].population) * 100
-//     });
-//   }
-// }
-
-
-
--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get('/location/:id', getAsteroidComparison)
 //TODO: put in a route for the asteroidFromAPI function here. Comment out when finished
 
 //error handler for invalid endpoint
-app.use('*', (req, res) => res.send('Sorry, an asteroid hit this route and it no longer exists'));
+// app.use('*', (req, res) => res.send('Sorry, an asteroid hit this route and it no longer exists'));
 
 //Error handler for 500 error
 function handleError(err, res, errorMessage){
@@ -120,7 +120,6 @@ function getAsteroidComparison(request, response){
 
   return client.query(sql,values)
     .then(result => {
-      console.log(result.rows[0]);
       return response.render('./pages/location',{asteroid: result.rows[0]});
     })
     .catch(error => handleError(error, response));
