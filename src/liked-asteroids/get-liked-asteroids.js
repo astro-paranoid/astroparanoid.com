6a61cf58521ca38a64f214e1da79bdb0c5a91af5
Jag <jagdeepsingh3939@gmail.com>
2019-07-31 13:48:34 -0700

    fixed error with liked page

--- src/liked-asteroids/get-liked-asteroids.js
+++ src/liked-asteroids/get-liked-asteroids.js
@@ -3,7 +3,7 @@ const handleError = require('../error/handle-error');
 
 // Gets all the liked asteroids from the database
 module.exports = (request, response) => {
-  const sql = `SELECT DISTINCT asteroids FROM asteroids JOIN liked ON liked.asteroid_id=asteroids.id;`;
+  const sql = `SELECT DISTINCT * FROM asteroids JOIN liked ON liked.asteroid_id=asteroids.id;`;
   client
     .query(sql)
     .then((sqlReturn) => response.render('pages/liked', { asteroids: sqlReturn.rows }))
